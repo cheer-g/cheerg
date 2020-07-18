0fb4b45fe13b9e575be3f1e6e1a4ccd790d7fca4
Sreerag E <sree.sreesree.rag@gmail.com>
2020-07-18 21:07:11 +0530

    Backend changes
    Added username in the php level and database level
    Removed authentication required to view the profile page
    Now it displays username in the nav bar instead of the name

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ProfilesController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
