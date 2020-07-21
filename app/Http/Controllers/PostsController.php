ffc79aeb59a76fe6908cf29b043437c59afddac4
Sreerag E <sree.sreesree.rag@gmail.com>
2020-07-21 11:36:15 +0530

    Adding new post successful
    You can now create a new post using the 'Add new post' button and it will show in the profile.
    Logo still broke

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -23,8 +23,14 @@ class PostsController extends Controller
             'image' => 'required | image',
         ]);
         
-        auth()->user()->posts()->create($data);
+        $imagePath = request('image')->store('uploads','public');
 
-        dd(request()->all());
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+        
+        return redirect('/profile/' . auth()->user()->id);
+        
     }
 }
