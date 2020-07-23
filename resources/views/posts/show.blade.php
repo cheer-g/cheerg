dedccd4af1853d6c6b4cbde8caa30c388c829a87
Sreerag E <sree.sreesree.rag@gmail.com>
2020-07-23 19:45:10 +0530

    Refined picture show view
    The picture show view now has tiny thumbnail of the user along with the username and caption. Clicking the username redirects to the corresponding profile

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,9 +8,27 @@
         </div>
         <div class="col-4">
             <div>
-                <h3>@ {{$post->user->username}} </h3>
+               <div class="d-flex align-items-center">
+                   <div class="pr-3">
+                        <img src="/storage/{{$post->user->profile->image}} " alt="{{$post->user->name}}'s dp " class="rounded-circle w-100" style="max-width: 45px;">
+                   </div>
+                   <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{$post->user->id}} ">
+                                <span class="text-dark">{{$post->user->username}}</span>
+                            </a>
+                        </div>
+                   </div>
+               </div>
             </div>
-            <p>{{$post->caption}} </p>
+            <hr>
+            <p class="pt-2">
+                <span class="font-weight-bold pr-2">
+                    <a href="/profile/{{$post->user->id}} ">
+                        <span class="text-dark">{{$post->user->username}}</span>
+                    </a>
+                </span>{{$post->caption}} 
+            </p>
         </div>
     </div>
 </div>
